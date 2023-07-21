9a762b32634045727a70865ba0b039c4d3badaaa
mamraj yadav <themamraj0131@gmail.com>
2023-07-21 22:47:20 +0530

    bug: updated code

--- _example/single-account/example.tf
+++ _example/single-account/example.tf
@@ -2,7 +2,7 @@
 ## Provider block added, Use the Amazon Web Services (AWS) provider to interact with the many resources supported by AWS.
 ##------------------------------------------------------------------------------
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
 }
 
 ##------------------------------------------------------------------------------
@@ -12,8 +12,8 @@ module "vpc" {
   source  = "clouddrove/vpc/aws"
   version = "1.3.1"
 
-  name        = "vpc"
-  environment = "test"
+  name        = "vpc1"
+  environment = "transit-gateway"
   label_order = ["environment", "name"]
   cidr_block  = "10.10.0.0/16"
 }
@@ -25,10 +25,10 @@ module "subnets" {
   source  = "clouddrove/subnet/aws"
   version = "1.3.0"
 
-  name                = "subnets"
-  environment         = "test"
+  name                = "subnets1"
+  environment         = "transit-gateway"
   label_order         = ["environment", "name"]
-  availability_zones  = ["eu-west-1a", "eu-west-1b"]
+  availability_zones  = ["eu-west-2a", "eu-west-2b"]
   vpc_id              = module.vpc.vpc_id
   type                = "public"
   igw_id              = module.vpc.igw_id
@@ -44,8 +44,8 @@ module "vpc-other" {
   source  = "clouddrove/vpc/aws"
   version = "1.3.1"
 
-  name        = "vpc"
-  environment = "test"
+  name        = "vpc2"
+  environment = "transit-gateway"
   label_order = ["environment", "name"]
 
   cidr_block = "192.168.0.0/16"
@@ -58,10 +58,10 @@ module "subnets-other" {
   source  = "clouddrove/subnet/aws"
   version = "1.3.0"
 
-  name                = "subnets"
-  environment         = "test"
+  name                = "subnets2"
+  environment         = "transit-gateway"
   label_order         = ["environment", "name"]
-  availability_zones  = ["eu-west-1a", "eu-west-1b"]
+  availability_zones  = ["eu-west-2a", "eu-west-2b"]
   vpc_id              = module.vpc-other.vpc_id
   type                = "public"
   igw_id              = module.vpc-other.igw_id
@@ -75,44 +75,40 @@ module "subnets-other" {
 ## transit-gateway module call.
 ##------------------------------------------------------------------------------
 module "transit-gateway" {
-  source = "./../../"
-
-  name        = "transit-gateway"
-  environment = "test"
-  label_order = ["environment", "name"]
-  enable      = true
-  tgw_create  = true
-
+  source                          = "./../../"
+  name                            = "transit-gateway"
+  environment                     = "test"
+  label_order                     = ["environment", "name"]
+  enable                          = true
+  tgw_create                      = true
   amazon_side_asn                 = 64512
   auto_accept_shared_attachments  = "enable"
   default_route_table_propagation = "enable"
   description                     = "This transit Gateway create for testing purpose"
-
   #TGW Share
   resource_share_enable                    = false
   resource_share_allow_external_principals = true
-  resource_share_account_ids               = ["XXXXXXXXXXXXX"]
-  subnet_ids                               = module.subnets.private_subnet_id
-
+  resource_share_account_ids               = ["924144197303"]
   # VPC Attachements
   vpc_attachement_create = false # Enable After once create the subnets
   vpc_id                 = module.vpc.vpc_id
+  subnet_ids             = module.subnets.public_subnet_id
   destination_cidr_block = ["192.168.0.0/16"]
 }
 
 ##------------------------------------------------------------------------------
 ## vpc-attachement module call.
 ##------------------------------------------------------------------------------
-module "vpc-attachement" {
+module "vpc-attachement-2" {
   source      = "./../../"
   name        = "transit-gateway"
   environment = "test"
   label_order = ["environment", "name"]
-
   # VPC Attachements
   vpc_id                          = module.vpc-other.vpc_id
-  destination_cidr_block          = ["10.10.0.0/16"]
+  destination_cidr_block          = ["10.20.0.0/16"]
   vpc_attachement_create          = false # Enable After once create the subnets
   use_existing_transit_gateway_id = true
   transit_gateway_id              = module.transit-gateway.transit_gateway_id
+  subnet_ids                      = module.subnets-other.public_subnet_id
 }
