8da4907caff6380aedfedb3998ddbd2cce8f5793
mamrajyadav <themamraj0131@gmail.com>
2023-10-10 22:21:36 +0530

    feat: add transit gateway route and update terraform version

--- _example/multi_account/slave_account/example.tf
+++ _example/multi_account/slave_account/example.tf
@@ -21,8 +21,8 @@ module "vpc" {
 ##------------------------------------------------------------------------------
 ## Subnet module call.
 ##------------------------------------------------------------------------------
-#tfsec:ignore:aws-ec2-no-excessive-port-access # Ingnored because these are basic examples, it can be changed via varibales as per requirement. 
-#tfsec:ignore:aws-ec2-no-public-ingress-acl # Ingnored because these are basic examples, it can be changed via varibales as per requirement. 
+#tfsec:ignore:aws-ec2-no-excessive-port-access # Ingnored because these are basic examples, it can be changed via varibales as per requirement.
+#tfsec:ignore:aws-ec2-no-public-ingress-acl # Ingnored because these are basic examples, it can be changed via varibales as per requirement.
 module "subnets" {
   source              = "clouddrove/subnet/aws"
   version             = "2.0.0"
@@ -30,7 +30,7 @@ module "subnets" {
   environment         = local.environment
   availability_zones  = ["eu-west-2a", "eu-west-2b"]
   vpc_id              = module.vpc.vpc_id
-  type                = "public"
+  type                = "private"
   igw_id              = module.vpc.igw_id
   nat_gateway_enabled = false
   cidr_block          = module.vpc.vpc_cidr_block
@@ -38,29 +38,30 @@ module "subnets" {
 }
 
 ##------------------------------------------------------------------------------
-## Transit-gateway module call for diff account. 
-## Transit gateway configuration for slave account. This account will share/use a central transit gateway hosted in main account. 
+## Transit-gateway module call for diff account.
+## Transit gateway configuration for slave account. This account will share/use a central transit gateway hosted in main account.
 ##------------------------------------------------------------------------------
 module "transit_gateway_peer" {
-  depends_on  = [module.vpc, module.subnets]
-  source      = "./../../../"
-  name        = local.name
-  environment = local.environment
-  tgw_create  = false
+  depends_on       = [module.vpc, module.subnets]
+  source           = "./../../../"
+  name             = local.name
+  environment      = local.environment
+  tgw_create       = false
+  hub_static_route = false
   #TGW Share
   aws_ram_resource_share_accepter = true
-  resource_share_arn              = "arn:aws:ram:eu-west-1:xxxxxxxxxx:resource-share/xxxxxxxxxxxxxxxxxxxxxxxxxx"
+  resource_share_arn              = "arn:aws:ram:eu-west-1:xxxxxxxx:resource-share/40b2b19b-6de6-478a-849b-xxxxxxxx"
   # VPC Attachements
-  transit_gateway_id = "tgw-xxxxxxxxxxx"
+  transit_gateway_id = "tgw-02c00xxxxxxx"
   vpc_attachments = {
     vpc1 = {
       vpc_id                                          = module.vpc.vpc_id
-      subnet_ids                                      = module.subnets.public_subnet_id
+      subnet_ids                                      = module.subnets.private_subnet_id
       transit_gateway_default_route_table_association = true
       transit_gateway_default_route_table_propagation = true
-      # Below should be uncommented only when vpc and subnet are already deployed.
-      #vpc_route_table_ids                            = module.subnets.public_route_tables_id
-      #destination_cidr                               = ["10.10.0.0/16"]
+      # Below should be uncommented only when vpc and subnet are already deployed.deployed
+      vpc_route_table_ids = module.subnets.private_route_tables_id
+      destination_cidr    = ["0.0.0.0/0", "10.10.0.0/16"]
     }
   }
 }
--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.5.4"
+  required_version = ">= 1.5.7"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.10"
+      version = ">= 5.13.1"
     }
   }
 }
