6add24c2c7f27f0b25a297e71f80c3494e65bcab
Chetan Kesare <chetan.kesare@clouddrove.com>
2026-05-06 00:45:34 +0530

    fix: changed provider meta

--- versions.tf
+++ versions.tf
@@ -10,6 +10,6 @@ terraform {
   }
 
   provider_meta "aws" {
-    module_name = "clouddrove/terraform-aws-transit-gateway"
+    user_agent = ["github.com/clouddrove/terraform-aws-transit-gateway"]
   }
 }
